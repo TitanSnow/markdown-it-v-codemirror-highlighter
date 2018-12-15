61878fd1ef168d2fe2f7e4a599e69ea576a84c10
TitanSnow <sweeto@live.cn>
2018-12-15 12:45:37 +0800

    add es module output

--- rollup.config.js
+++ rollup.config.js
@@ -12,18 +12,30 @@ const watch = [
 export default [
     {
         input: 'src/nodeIndex.js',
-        output: {
-            file: 'dist/nodeIndex.common.js',
-            format: 'cjs'
-        },
+        output: [
+            {
+                file: 'dist/nodeIndex.common.js',
+                format: 'cjs'
+            },
+            {
+                file: 'dist/nodeIndex.esm.js',
+                format: 'es'
+            },
+        ],
         external, watch
     },
     {
         input: 'src/browserIndex.js',
-        output: {
-            file: 'dist/browserIndex.common.js',
-            format: 'cjs'
-        },
+        output: [
+            {
+                file: 'dist/browserIndex.common.js',
+                format: 'cjs'
+            },
+            {
+                file: 'dist/browserIndex.esm.js',
+                format: 'es'
+            },
+        ],
         external, watch
     }
 ]
